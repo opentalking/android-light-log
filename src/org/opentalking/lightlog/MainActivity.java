82ab30490d2b6068f787a557fd70f96ff41fa4ce
Haoxiqiang <haoxiqiang@live.com>
2014-08-01 22:37:54 +0800

    1.fix bugs

--- src/org/opentalking/lightlog/MainActivity.java
+++ src/org/opentalking/lightlog/MainActivity.java
@@ -2,7 +2,6 @@ package org.opentalking.lightlog;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.KeyEvent;
 
 public class MainActivity extends Activity {
 
@@ -10,41 +9,66 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ILog.v("level v : Activity Life Cycle");
+		ILog.i("level i : Activity Life Cycle");
+		ILog.d("level d : Activity Life Cycle");
+		ILog.w("level w : Activity Life Cycle");
+		ILog.e("level e : Activity Life Cycle");
+		ILog.wtf("level wtf : Activity Life Cycle");
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
-		ILog.i("Activity Life Cycle");
+		ILog.v("level v : Activity Life Cycle");
+		ILog.i("level i : Activity Life Cycle");
+		ILog.d("level d : Activity Life Cycle");
+		ILog.w("level w : Activity Life Cycle");
+		ILog.e("level e : Activity Life Cycle");
+		ILog.wtf("level wtf : Activity Life Cycle");
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		ILog.i("Activity Life Cycle");
+		ILog.v("level v : Activity Life Cycle");
+		ILog.i("level i : Activity Life Cycle");
+		ILog.d("level d : Activity Life Cycle");
+		ILog.w("level w : Activity Life Cycle");
+		ILog.e("level e : Activity Life Cycle");
+		ILog.wtf("level wtf : Activity Life Cycle");
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-		ILog.i("Activity Life Cycle");
+		ILog.v("level v : Activity Life Cycle");
+		ILog.i("level i : Activity Life Cycle");
+		ILog.d("level d : Activity Life Cycle");
+		ILog.w("level w : Activity Life Cycle");
+		ILog.e("level e : Activity Life Cycle");
+		ILog.wtf("level wtf : Activity Life Cycle");
 	}
 
 	@Override
 	protected void onStop() {
 		super.onStop();
-		ILog.i("Activity Life Cycle");
+		ILog.v("level v : Activity Life Cycle");
+		ILog.i("level i : Activity Life Cycle");
+		ILog.d("level d : Activity Life Cycle");
+		ILog.w("level w : Activity Life Cycle");
+		ILog.e("level e : Activity Life Cycle");
+		ILog.wtf("level wtf : Activity Life Cycle");
 	}
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-		ILog.i("Activity Life Cycle");
+		ILog.v("level v : Activity Life Cycle");
+		ILog.i("level i : Activity Life Cycle");
+		ILog.d("level d : Activity Life Cycle");
+		ILog.w("level w : Activity Life Cycle");
+		ILog.e("level e : Activity Life Cycle");
+		ILog.wtf("level wtf : Activity Life Cycle");
 	}
-
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		return super.onKeyDown(keyCode, event);
-	}
-
 }
